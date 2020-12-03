18f3dcd81fb7b8b53975eaa37d4e65840eb4b182
Javier Costanzi <javier.costanzi@d3sistemas.com.ar>
2020-12-03 12:08:01 -0600

    Install clean and concat gulp plugins. Add style and js basic tasks using concat.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
-function defaultTask(cb) {
-  // place code for your default task here
-  cb();
+const { src, dest, series } = require('gulp');
+const clean = require('gulp-clean');
+const concat = require('gulp-concat');
+
+function cleanTask(cb) {
+  return src('dist/', { read: false, allowEmpty: true })
+    .pipe(clean());
 }
 
-exports.default = defaultTask
+function styleTask(cb) {
+  return src('src/css/*')
+    .pipe(concat('all-styles.css'))
+    .pipe(dest('dist/css'));
+}
+
+function jsTask(cb) {
+  return src('src/js/*')
+    .pipe(concat('all-js.js'))
+    .pipe(dest('dist/js'));
+}
+
+exports.default = series(cleanTask, styleTask, jsTask);
